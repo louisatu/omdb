a03b6ba4bee71a2dca9ad856e1d030ec7eea904e
azizmoalim <43540550+abdiaziz@users.noreply.github.com>
2020-08-02 14:42:46 -0500

    Update omdb_sql_queries_iteration10.sql.sql

--- sql/omdb_sql_queries_iteration10.sql.sql
+++ sql/omdb_sql_queries_iteration10.sql.sql
@@ -3,10 +3,13 @@ By: Aziz Moalim
 7.43
 Give me a summary of m_link_type. The result set should contain the count of each m_link_type. 
 
-1,175,493 rows
+This is the over 100,000 data set:
+This is used on 1,175,493 rows and returns the below time
 Showing rows 0 - 2 (3 total, Query took 0.8430 seconds.)
 
-18 rows
+This is the orginal data:
+
+This is used on 18 rows and returns the below time
 Showing rows 0 - 2 (3 total, Query took 0.0011 seconds.)
 
 
